d028d04de9c68a64329e3b7ad3c41afe3a8f8447
nahyan0077 <nahyanmp4@gmail.com>
2024-09-19 15:54:03 +0530

    feat: folder structure setup

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 export const FeedbackForm: React.FC = () => {
   return (
-    <div className='min-h-screen mx-auto max-w-7xl p-10 ' >
+    <div className=' ' >
 
     </div>
   )
